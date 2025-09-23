835115bd3cc6887747a67f13f6672c1bc3df00b9
sachindw12082000 <sachindw1208@gmail.com>
2025-09-23 12:32:06 +0530

    new code added at 23-09-2025

--- src/test/java/com/stream/stream/StreamApplicationTests.java
+++ src/test/java/com/stream/stream/StreamApplicationTests.java
@@ -1,13 +1,28 @@
 package com.stream.stream;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StreamApplicationTests {
+public	StreamApplication mockApp = Mockito.mock(StreamApplication.class);
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	 void TestReturnAmount(){
+		int result=1;
+when(mockApp.returnAmount(1)).thenReturn(result);
+
+assertEquals(1,result);
+
+
+
+	}
 }
